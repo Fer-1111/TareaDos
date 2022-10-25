ebc05b71625e1885d0e68c405a7e9305a2fdb158
seoz2003 <109814429+seoz2003@users.noreply.github.com>
2022-10-25 15:37:22 -0300

    el main con el comprador

---
+++ codigo/Comprador.java
@@ -0,0 +1,14 @@
+package tarea2;
+
+public class Comprador{
+    private String tipoBebida;
+    private int serie;
+    
+    public Comprador(Moneda m, int cualBebida, Expendedor exp){
+        
+    }
+    public int cuantoVuelto(){ //de vuelto        
+    }
+    public String queBebiste(){//el sonido de la Bebida: cocacola, sprite
+    }
+}
---
+++ codigo/Tarea2.java
@@ -0,0 +1,13 @@
+package tarea2;
+
+import java.util.ArrayList;
+
+public class Tarea2 {
+
+    public static void main(String[] args) {
+        Moneda luka = new Moneda1000();
+        Expendedor n1 = new Expendedor(1,1000);
+        Comprador cliente = new Comprador(luka, 1, n1);
+    }
+    
+}
