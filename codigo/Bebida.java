118158a15ba454dfeb63d9c25455eb98b492a094
JorgeAF23 <joavendano2021@udec.cl>
2022-10-25 15:58:08 -0300

    agregue bebida y deposito

---
+++ codigo/Bebida.java
@@ -0,0 +1,56 @@
+package tarea2;
+
+public abstract class Bebida{
+    public Bebida(int numSerie){
+     
+    }
+    public int getSerie(){
+    }
+    public abstract String beber();
+}
+class Sprite extends Bebida {
+
+    public Sprite(int serie) {
+        super(serie);
+    }
+
+    public String beber() {
+        return "sprite";
+    }
+
+}
+class CocaCola extends Bebida {
+
+    public CocaCola(int serie) {
+        super(serie);
+    }
+
+    public String beber() {
+        return "fanta";
+    }
+
+}
+class Fanta extends Bebida {
+
+    public Fanta(int serie) {
+        super(serie);
+    }
+
+    public String beber() {
+        return "fanta";
+    }
+
+}
+
+class Kem extends Bebida {
+
+    public Kem(int serie) {
+        super(serie);
+    }
+
+    public String beber() {
+        return "kem";
+    }
+
+}
+
