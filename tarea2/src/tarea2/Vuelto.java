29c1e682ba7a5035384607a03cc0022a9ae2c1c0
seoz2003 <109814429+seoz2003@users.noreply.github.com>
2022-10-25 21:59:46 -0300

    metodo vuelto

---
+++ tarea2/src/tarea2/Vuelto.java
@@ -0,0 +1,11 @@
+package tarea2;
+
+import java.util.ArrayList;
+
+public class Vuelto {
+    private ArrayList<Moneda> monedasVuelto;
+
+    public Vuelto() {
+        monedasVuelto = new ArrayList<Moneda>();
+    }    
+}
