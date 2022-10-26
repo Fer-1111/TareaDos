8a8488798a4e62c0621f7ee437b7a1103b883e3e
seoz2003 <sergio.opazo.zamorano@gmail.com>
2022-10-26 15:18:18 -0300

    no importante

--- tarea2/src/tarea2/Comprador.java
+++ tarea2/src/tarea2/Comprador.java
@@ -5,12 +5,25 @@ public class Comprador{
     private int serie;
     
     public Comprador(Moneda m, int cualBebida, Expendedor exp){
+        if(cualBebida == 1){
+            tipoBebida = "CocaCola";
+        }
+        if(cualBebida == 2){
+            tipoBebida = "Sprite";
+        }
+        if(cualBebida == 3){
+            tipoBebida = "Fanta";
+        }
+        if(cualBebida == 4){
+            tipoBebida = "Kem";
+        }
         
+    
     }
     public int cuantoVuelto(){ //de vuelto        
-        return 0;
+        return 200;
     }
     public String queBebiste(){//el sonido de la Bebida: cocacola, sprite
-        return null;
+        
     }
 }
